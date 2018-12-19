1b864e2ed7066a1330d369516e119b80e4cf407b
colin axner <colinaxner@berkeley.edu>
2018-12-18 23:44:33 -0800

    Update auto
    works now yeah

--- auto.sh
+++ auto.sh
@@ -1,22 +1,30 @@
-#!/bin/bash
-# Script For Auto-Delegate from validator kreios 
+#!/bin/bash -x
+# Script For Auto-Delegate forked from validator kreios
 while true
         do
-                #replace cosmos1qm7knjdln0ap9fpykdu5rcajm9cf2esvp8ey2v with your own cosmosaddr
-                STEAK=`sudo -u gaiad /opt/go/bin/gaiacli query account --chain-id=genki-2000 cosmos1pfl3pwpf9s46myukhv6muu6zpm7gtlpsszu2m0 --trust-node --home=/opt/gaiad | jq ".value.coins" | jq ".[0].amount" | bc`
                 passphrase=""
-                echo ""
-                echo "Got ""$((STEAK - 1))"" Reward-STAKE"
-                Zahl="$((STEAK - 1))"
-                #replace --from "main" with your own keyname and cosmosvaloper1qm7knjdln0ap9fpykdu5rcajm9cf2esvynd3xl with the output from gaiacli keys show yourkeyname --bech=val
+                echo $passphrase|sudo -u gaiad /opt/go/bin/gaiacli tx dist withdraw-rewards --chain-id "game_of_stakes" --from "mykey" --is-validator --home=/opt/gaiad
+                STEAK=`sudo -u gaiad /opt/go/bin/gaiacli query account --chain-id=game_of_stakes cosmos1pfl3pwpf9s46myukhv6muu6zpm7gtlpsszu2m0 --trust-node --home=/opt/gaiad | jq ".value.coins" | jq ".[0].amount" | bc`
+                while [ "$STEAK" != "0" ]
+                        do
+                                echo ""
+                                echo "Got ""$((STEAK ))"" Reward-STAKE"
+                                echo ""
+                                echo $passphrase|sudo -u gaiad /opt/go/bin/gaiacli tx dist withdraw-rewards --chain-id "game_of_stakes" --from "mykey" --is-validator --home=/opt/gaiad
+                                SEQ=`sudo -u gaiad /opt/go/bin/gaiacli query account --chain-id=game_of_stakes cosmos1pfl3pwpf9s46myukhv6muu6zpm7gtlpsszu2m0 --trust-node --home=/opt/gaiad | jq ".value.sequence" | bc`
 
-                echo $passphrase|sudo -u gaiad /opt/go/bin/gaiacli tx stake delegate --from "mykey" --validator "cosmosvaloper1pfl3pwpf9s46myukhv6muu6zpm7gtlps4kglhu" --chain-id "genki-2000" --amount "$Zahl""STAKE" --home="/opt/gaiad"
-                sleep 6s
+                                echo $passphrase|sudo -u gaiad /opt/go/bin/gaiacli tx stake delegate --chain-id "game_of_stakes" --from "mykey" --validator "cosmosvaloper1pfl3pwpf9s46myukhv6muu6zpm7gtlps4kglhu" --amount "$STEAK""STAKE" --account-number 125 --sequence $(($SEQ + 1)) --home=/opt/gaiad
+                                sleep 10s
+                                echo ""
+                                sudo -u gaiad /opt/go/bin/gaiacli status | jq ".[].voting_power"
+                                echo ""
+                                STEAK=`sudo -u gaiad /opt/go/bin/gaiacli query account --chain-id=game_of_stakes cosmos1pfl3pwpf9s46myukhv6muu6zpm7gtlpsszu2m0 --trust-node --home=/opt/gaiad | jq ".value.coins" | jq ".[0].amount" | bc`
+                                echo "$STEAK"
+                                sleep 11s
+                done
+                echo""
+                echo "You only have $STEAK STAKE"
                 echo ""
-                echo ""
-                gaiacli status | jq ".[].voting_power"
-                echo ""
-                echo ""
-                sleep 4s
+                sleep 12000
         done
 
