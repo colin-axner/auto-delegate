81e9adab34ab075925a376d6cb1c1ffdb0a64efc
Colin Axner <colinaxner@berkeley.edu>
2018-12-21 12:58:26 -0800

    fixed auto delegation

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 This scripts needs the packages jq and bc to function.
 Install under ubuntu with sudo apt-get install jq bc
 
-Add your Passphrase as String, cosmosvaloper addres(gaiacli keys show keyname --bech=val), your cosmosaddr and save as auto.sh then auto chmod a + x auto.sh and then run it with ./auto.sh
+Add your Passphrase as String, cosmosvaloper addres(gaiacli keys show keyname --bech=val), your cosmosaddr and save as auto.sh then `chmod +x auto.sh` and then run `./auto.sh`
+
+It will query for STEAK you have unbonded, call them to be distributed, get your next sequence number, and then bond them.
--- auto.sh
+++ auto.sh
@@ -1,26 +1,26 @@
 #!/bin/bash -x
-# Script For Auto-Delegate forked from validator kreios
+# Script For Auto-Delegate from validator kreios
 while true
         do
-                passphrase=""
+		passphrase= ""
                 echo $passphrase|sudo -u gaiad /opt/go/bin/gaiacli tx dist withdraw-rewards --chain-id "game_of_stakes" --from "mykey" --is-validator --home=/opt/gaiad
                 STEAK=`sudo -u gaiad /opt/go/bin/gaiacli query account --chain-id=game_of_stakes cosmos1pfl3pwpf9s46myukhv6muu6zpm7gtlpsszu2m0 --trust-node --home=/opt/gaiad | jq ".value.coins" | jq ".[0].amount" | bc`
-                while [ "$STEAK" != "0" ]
+		while [ "$STEAK" != "0" ]
                         do
                                 echo ""
                                 echo "Got ""$((STEAK ))"" Reward-STAKE"
                                 echo ""
-                                echo $passphrase|sudo -u gaiad /opt/go/bin/gaiacli tx dist withdraw-rewards --chain-id "game_of_stakes" --from "mykey" --is-validator --home=/opt/gaiad
-                                SEQ=`sudo -u gaiad /opt/go/bin/gaiacli query account --chain-id=game_of_stakes cosmos1pfl3pwpf9s46myukhv6muu6zpm7gtlpsszu2m0 --trust-node --home=/opt/gaiad | jq ".value.sequence" | bc`
+				echo $passphrase|sudo -u gaiad /opt/go/bin/gaiacli tx dist withdraw-rewards --chain-id "game_of_stakes" --from "mykey" --is-validator --home=/opt/gaiad
+				SEQ=`sudo -u gaiad /opt/go/bin/gaiacli query account --chain-id=game_of_stakes cosmos1pfl3pwpf9s46myukhv6muu6zpm7gtlpsszu2m0 --trust-node --home=/opt/gaiad | jq ".value.sequence" | bc`
 
-                                echo $passphrase|sudo -u gaiad /opt/go/bin/gaiacli tx stake delegate --chain-id "game_of_stakes" --from "mykey" --validator "cosmosvaloper1pfl3pwpf9s46myukhv6muu6zpm7gtlps4kglhu" --amount "$STEAK""STAKE" --account-number 125 --sequence $(($SEQ + 1)) --home=/opt/gaiad
+				echo $passphrase|sudo -u gaiad /opt/go/bin/gaiacli tx stake delegate --chain-id "game_of_stakes" --from "mykey" --validator "cosmosvaloper1pfl3pwpf9s46myukhv6muu6zpm7gtlps4kglhu" --amount "$STEAK""STAKE" --account-number 125 --sequence $(($SEQ + 1)) --home=/opt/gaiad
                                 sleep 10s
                                 echo ""
                                 sudo -u gaiad /opt/go/bin/gaiacli status | jq ".[].voting_power"
                                 echo ""
-                                STEAK=`sudo -u gaiad /opt/go/bin/gaiacli query account --chain-id=game_of_stakes cosmos1pfl3pwpf9s46myukhv6muu6zpm7gtlpsszu2m0 --trust-node --home=/opt/gaiad | jq ".value.coins" | jq ".[0].amount" | bc`
-                                echo "$STEAK"
-                                sleep 11s
+				STEAK=`sudo -u gaiad /opt/go/bin/gaiacli query account --chain-id=game_of_stakes cosmos1pfl3pwpf9s46myukhv6muu6zpm7gtlpsszu2m0 --trust-node --home=/opt/gaiad | jq ".value.coins" | jq ".[0].amount" | bc`
+				echo "$STEAK"
+                                sleep 100s
                 done
                 echo""
                 echo "You only have $STEAK STAKE"
